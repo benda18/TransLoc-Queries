1a9691721010b2ac9840e789e9bdcb9a5b47e5e9
benda18 <bendertj@gmail.com>
2014-11-12 08:50:45 -0500

    Update BusSpeed_RLine.rb

--- BusSpeed_RLine.rb
+++ BusSpeed_RLine.rb
@@ -6,7 +6,7 @@ somefile = File.open(fname, "w")
 
 response = Unirest.get "https://transloc-api-1-2.p.mashape.com/vehicles.jsonp?agencies=20&callback=call&routes=4000032",
   headers:{
-    "X-Mashape-Key" => "5SA6zZbMxpmshjhL0Y8ERCZ2WqnIp1zgKOmjsnXy5WDKmZaGC4"
+    "X-Mashape-Key" => "<key>"
   }
 
 
