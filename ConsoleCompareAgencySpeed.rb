100a771f500f35b9160cdb6ed20a62a958ab407d
benda18 <bendertj@gmail.com>
2014-11-12 13:10:27 -0500

    Update ConsoleCompareAgencySpeed.rb

--- ConsoleCompareAgencySpeed.rb
+++ ConsoleCompareAgencySpeed.rb
@@ -4,12 +4,10 @@
 #	xx3. for multiple agencies
 # 	xx4. and is refreshed every (d) seconds
 #	xx5. tracks moving avg fleet speed
-# 	6. writes somefile.txt  with a header <time>,<agency>,<count>,<avg speed>
-#	7. populates somefile.txt with  data
-
-
-
-
+# 	xx6. writes somefile.txt  with a header <time>,<agency>,<count>,<avg speed>
+#	xx7. populates somefile.txt with  data
+#	xx8. longitude and latitude
+# 	xx9. log file
 
 #REQUIRE STATEMENTS
 require 'unirest'
@@ -17,8 +15,9 @@ require 'json'
 
 #WRITE TO CONSOLE
 puts "working"	#script is running
-fname = "AgencySpeedData #{Time.now.strftime('%Y%m%d-%H%M%S')}.txt"		#Creates a new .txt file in directory of .rb
-#somefile = File.open(fname, "w")									#Opens a writable copy of fname.txt
+fname = "AgencySpeed_avg #{Time.now.strftime('%Y%m%d-%H%M%S')}.txt"		#Creates a new .txt file in directory of .rb
+gname = "AgencySpeed_bus #{Time.now.strftime('%Y%m%d-%H%M%S')}.txt"		#Creates a new .txt file in directory of .rb
+hname = "AgencySpeed_log #{Time.now.strftime('%Y%m%d-%H%M%S')}.txt"		#Creates a new .txt file in directory of .rb
 puts 'How long to sleep (in seconds) between each loop? (typically ~5)'
 d = gets.to_i 															#delay in seconds between each repeat iteration
 puts "\e[H\e[2J"
@@ -39,12 +38,6 @@ varCcumCt = 0
 varTcum = 0		
 varCcum = 0
 
-
-
-
-
-
-
 #LOOP UNTIL
 varI = 0		#variable that represents starting integer for loop count
 puts "\e[H\e[2J"
@@ -55,26 +48,27 @@ puts "\e[H\e[2J"
 puts "#{varNum} loops"
 sleep(2)
 
-
-#WRITE TO CONSOLE THE INPUTS
-#puts "Variable Inputs:" 			#variable inputs
-#puts "Loop delay: #{d} seconds" 	#loop delay in seconds
-#puts "Iterations: #{varNum}"		#count of loops to perform
-#puts "#{varI} of #{varNum}"			#x of y count
-
 #SOMEFILE HEADER
-#somefile.puts "time,agency,count,speed"
-
 File.open(fname, "a+") do |f1|
-	f1.puts "time,agency,count,speed"
+	f1.puts "time,agency,bus_id,speed,lon,lat"
+end
+File.open(gname, "a+") do |g7|
+	g7.puts "time,agency,count_buses,speed"
+end
+File.open(hname, "a+") do |h2|
+	h2.puts "###CONSOLE LOG FILE###"
+	h2.puts ""
+	h2.puts ""
+	h2.puts "How long to sleep (in seconds) between each loop? (typically ~5)"
+	h2.puts "#{d} seconds delay"
+	h2.puts "How many loops to perform? (typically ~10)"
+	h2.puts "#{varNum} loops"
 end
 
 #BEGIN FIRST ITERATION
 begin
 # variables
-
 varI +=1;
-
 varTime = "#{Time.now.strftime('%H')}:#{Time.now.strftime('%M')}:#{Time.now.strftime('%S')}"
 varCountt = 0
 varCount = 0 														#Counts the number of buses that respond during 
@@ -90,37 +84,47 @@ varAvg = 0															#A value derived from varSum divided by varCount
 # the mashape call to TransLoc api
 response = Unirest.get "https://transloc-api-1-2.p.mashape.com/vehicles.jsonp?agencies=12%2C20&callback=call",
   headers:{
-    "X-Mashape-Key" => "<key>"
+    "X-Mashape-Key" => "5SA6zZbMxpmshjhL0Y8ERCZ2WqnIp1zgKOmjsnXy5WDKmZaGC4"
   }
 #parse the call  
 payload2 = response.body				#sets the call response body as a variable
 payload = payload2[/{.+}/]				#removes the callback prefix and suffix from the response body
 data_hash = JSON.parse(payload)			#parses the response body and stores as a variable
 
-
 #loop through CAT parsed data to get the data you want
 data_hash["data"]["20"].each do |ary|	#for each object in the array,
 varSpd = ary["speed"]					#set var for "speed" value in array,
+varVID = ary["vehicle_id"]				#set var for "vehicle_id" value in array,
+varLng = ary["location"]["lng"]			#set var for longitude value in array,
+varLat = ary["location"]["lat"]			#set var for latitude value in array,
 varCount = varCount + 1					#increase repeat iterations by 1,
 varSum = varSum + ary["speed"]			#separately, set var for sum of all "speed" values in array,
 varCcumSpd = varCcumSpd + varSpd		#cumulative speed tracking
 varCcumCt = varCcumCt + 1				#cumulative count tracking
+
+
+
+File.open(fname, "a+") do |f4|
+	f4.puts "#{Time.now.strftime('%H')}:#{Time.now.strftime('%M')}:#{Time.now.strftime('%S')},CAT,#{varVID},#{varSpd},#{varLng},#{varLat}"
+end
 end										#...end array loop
-
-
 
 #loop through TTA parsed data to get the data you want
 data_hash["data"]["12"].each do |aryt|	#for each object in the array,
 varSpdt = aryt["speed"]					#set var for "speed" value in array,
+varVIDt = aryt["vehicle_id"]			#set var for "vehicle_id" value in array,
+varLngt = aryt["location"]["lng"]			#set var for longitude value in array,
+varLatt = aryt["location"]["lat"]			#set var for latitude value in array,
 varCountt = varCountt + 1				#increase repeat iterations by 1,
 varSumt = varSumt + aryt["speed"]		#separately, set var for sum of all "speed" values in array,
-varTcumSpd = varTcumSpd + varSpdt	#cumulative speed tracking
+varTcumSpd = varTcumSpd + varSpdt		#cumulative speed tracking
 varTcumCt = varTcumCt + 1				#cumulative count tracking
+File.open(fname, "a+") do |f5|
+	f5.puts "#{Time.now.strftime('%H')}:#{Time.now.strftime('%M')}:#{Time.now.strftime('%S')},TTA,#{varVIDt},#{varSpdt},#{varLngt},#{varLatt}"
+end
 end										#...end array loop
 
-
 #write the data in the console
-#somefile.puts "Average MPH"				#write "Average MPH" header to the .txt file
 varAvg = varSum / varCount				#calculate CAT instant average bus speed
 varAvgt = varSumt / varCountt			#calculate TTA instant average bus speed
 varTcum = varTcumSpd / varTcumCt		#calculate TTA cumulative avgerage bus speed
@@ -136,28 +140,26 @@ puts "Loop delay: #{d} seconds" 			#loop delay in seconds
 puts "Iterations: #{varI} of #{varNum+1}"		#x of y count
 
 #WRITE TO SOMEFILE
-#somefile.puts "#{varTime},CAT,#{varCount},#{varAvg}"
-File.open(fname, "a+") do |f2|
-	f2.puts "#{varTime},CAT,#{varCount},#{varAvg}"
+File.open(gname, "a+") do |g2|
+	g2.puts "#{varTime},CAT,#{varCount},#{varAvg}"
 end
-#somefile.puts "#{varTime},TTA,#{varCountt},#{varAvgt}"
-File.open(fname, "a+") do |f3|
-	f3.puts "#{varTime},TTA,#{varCountt},#{varAvgt}"
+File.open(gname, "a+") do |g3|
+	g3.puts "#{varTime},TTA,#{varCountt},#{varAvgt}"
+end
+File.open(hname, "a+") do |h1|
+	h1.puts ""
+	h1.puts ""
+	h1.puts "Iteration: #{varI} of #{varNum+1}"		#x of y count
+	h1.puts "Current Time: #{varTime}"				#current time
+	h1.puts "#{e} #{varAvg.round(1)} (#{varCcum.round(1)} cumulative)"
+	h1.puts "#{f} #{varAvgt.round(1)} (#{varTcum.round(1)} cumulative)"
 end
 
-#somefile.puts varAvg.round(1)			#write the average bus speed to the .txt file
-# log to console
-# b = b + 1								#represents the nth repeat iteration
-# puts b									#writes nth repeat iteration to console 
 
 sleep(d)
-
-
 end until varI > varNum
 
-
 #CLEANUP
-#somefile.close							#closes .txt file
 puts c									#writes "done" to console
-sleep(4)								#waits 10 seconds before closing console
+sleep(2)								#waits 10 seconds before closing console
 exit
