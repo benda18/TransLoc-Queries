308afd6b5635b0bb94a790213806e66543fdc3bd
benda18 <bendertj@gmail.com>
2014-11-12 13:18:30 -0500

    Update ConsoleCompareAgencySpeed.rb

--- ConsoleCompareAgencySpeed.rb
+++ ConsoleCompareAgencySpeed.rb
@@ -4,8 +4,8 @@
 #	xx3. for multiple agencies
 # 	xx4. and is refreshed every (d) seconds
 #	xx5. tracks moving avg fleet speed
-# 	xx6. writes somefile.txt  with a header <time>,<agency>,<count>,<avg speed>
-#	xx7. populates somefile.txt with  data
+# 	xx6. writes several log and data files
+#	xx7. populates files with data
 #	xx8. longitude and latitude
 # 	xx9. log file
 
@@ -27,7 +27,7 @@ sleep(2)
 
 #INITIAL VARIABLES
 b = 0
-c = "done"		#displayed in console to show that script is complete
+c = "done"				#displayed in console to show that script is complete
 e = "CAT mph = "
 f = "TTA mph = "
 
@@ -39,11 +39,11 @@ varTcum = 0
 varCcum = 0
 
 #LOOP UNTIL
-varI = 0		#variable that represents starting integer for loop count
+varI = 0				#variable that represents starting integer for loop count
 puts "\e[H\e[2J"
 puts 'How many loops to perform? (typically ~10)'
 varNum = gets.to_i         												#tracks number of repeat iterations, resets 
-																	# 	value to 0 
+																		#	value to 0 
 puts "\e[H\e[2J"
 puts "#{varNum} loops"
 sleep(2)
@@ -71,73 +71,71 @@ begin
 varI +=1;
 varTime = "#{Time.now.strftime('%H')}:#{Time.now.strftime('%M')}:#{Time.now.strftime('%S')}"
 varCountt = 0
-varCount = 0 														#Counts the number of buses that respond during 
-																	#	each iteration
-varSumt = 0																	
-varSum = 0 															#The sum of the speed values for each responding 
-																	#	bus during each iteration
-varAvgt = 0
-varAvg = 0															#A value derived from varSum divided by varCount 
-																	#	that equals the average speed for all responding 
-																	#	buses during each iteration
+varCount = 0 								#Counts the number of buses that respond during 
+											#	each iteration
+varSumt = 0											
+varSum = 0 									#The sum of the speed values for each responding 
+											#	bus during each iteration
+varAvgt = 0	
+varAvg = 0									#A value derived from varSum divided by varCount 
+											#	that equals the average speed for all responding 
+											#	buses during each iteration
 
 # the mashape call to TransLoc api
 response = Unirest.get "https://transloc-api-1-2.p.mashape.com/vehicles.jsonp?agencies=12%2C20&callback=call",
   headers:{
-    "X-Mashape-Key" => "<key>"
+    "X-Mashape-Key" => "5SA6zZbMxpmshjhL0Y8ERCZ2WqnIp1zgKOmjsnXy5WDKmZaGC4"
   }
 #parse the call  
-payload2 = response.body				#sets the call response body as a variable
-payload = payload2[/{.+}/]				#removes the callback prefix and suffix from the response body
-data_hash = JSON.parse(payload)			#parses the response body and stores as a variable
+payload2 = response.body					#sets the call response body as a variable
+payload = payload2[/{.+}/]					#removes the callback prefix and suffix from the response body
+data_hash = JSON.parse(payload)				#parses the response body and stores as a variable
 
 #loop through CAT parsed data to get the data you want
-data_hash["data"]["20"].each do |ary|	#for each object in the array,
-varSpd = ary["speed"]					#set var for "speed" value in array,
-varVID = ary["vehicle_id"]				#set var for "vehicle_id" value in array,
-varLng = ary["location"]["lng"]			#set var for longitude value in array,
-varLat = ary["location"]["lat"]			#set var for latitude value in array,
-varCount = varCount + 1					#increase repeat iterations by 1,
-varSum = varSum + ary["speed"]			#separately, set var for sum of all "speed" values in array,
-varCcumSpd = varCcumSpd + varSpd		#cumulative speed tracking
-varCcumCt = varCcumCt + 1				#cumulative count tracking
+data_hash["data"]["20"].each do |ary|		#for each object in the array,
+varSpd = ary["speed"]						#set var for "speed" value in array,
+varVID = ary["vehicle_id"]					#set var for "vehicle_id" value in array,
+varLng = ary["location"]["lng"]				#set var for longitude value in array,
+varLat = ary["location"]["lat"]				#set var for latitude value in array,
+varCount = varCount + 1						#increase repeat iterations by 1,
+varSum = varSum + ary["speed"]				#separately, set var for sum of all "speed" values in array,
+varCcumSpd = varCcumSpd + varSpd			#cumulative speed tracking
+varCcumCt = varCcumCt + 1					#cumulative count tracking
 
-
-
+#write it to a file
 File.open(fname, "a+") do |f4|
 	f4.puts "#{Time.now.strftime('%H')}:#{Time.now.strftime('%M')}:#{Time.now.strftime('%S')},CAT,#{varVID},#{varSpd},#{varLng},#{varLat}"
 end
-end										#...end array loop
+end											#...end array loop
 
 #loop through TTA parsed data to get the data you want
-data_hash["data"]["12"].each do |aryt|	#for each object in the array,
-varSpdt = aryt["speed"]					#set var for "speed" value in array,
-varVIDt = aryt["vehicle_id"]			#set var for "vehicle_id" value in array,
+data_hash["data"]["12"].each do |aryt|		#for each object in the array,
+varSpdt = aryt["speed"]						#set var for "speed" value in array,
+varVIDt = aryt["vehicle_id"]				#set var for "vehicle_id" value in array,
 varLngt = aryt["location"]["lng"]			#set var for longitude value in array,
 varLatt = aryt["location"]["lat"]			#set var for latitude value in array,
-varCountt = varCountt + 1				#increase repeat iterations by 1,
-varSumt = varSumt + aryt["speed"]		#separately, set var for sum of all "speed" values in array,
-varTcumSpd = varTcumSpd + varSpdt		#cumulative speed tracking
-varTcumCt = varTcumCt + 1				#cumulative count tracking
+varCountt = varCountt + 1					#increase repeat iterations by 1,
+varSumt = varSumt + aryt["speed"]			#separately, set var for sum of all "speed" values in array,
+varTcumSpd = varTcumSpd + varSpdt			#cumulative speed tracking
+varTcumCt = varTcumCt + 1					#cumulative count tracking
 File.open(fname, "a+") do |f5|
 	f5.puts "#{Time.now.strftime('%H')}:#{Time.now.strftime('%M')}:#{Time.now.strftime('%S')},TTA,#{varVIDt},#{varSpdt},#{varLngt},#{varLatt}"
-end
-end										#...end array loop
-
-#write the data in the console
-varAvg = varSum / varCount				#calculate CAT instant average bus speed
-varAvgt = varSumt / varCountt			#calculate TTA instant average bus speed
-varTcum = varTcumSpd / varTcumCt		#calculate TTA cumulative avgerage bus speed
-varCcum = varCcumSpd / varCcumCt		#calculate CAT cumulative average bus speed
-
-puts "\e[H\e[2J"
+end	
+end											#...end array loop
+	
+#write the data in the console	
+varAvg = varSum / varCount					#calculate CAT instant average bus speed
+varAvgt = varSumt / varCountt				#calculate TTA instant average bus speed
+varTcum = varTcumSpd / varTcumCt			#calculate TTA cumulative avgerage bus speed
+varCcum = varCcumSpd / varCcumCt			#calculate CAT cumulative average bus speed
+puts "\e[H\e[2J"							#clears console
 puts "Current Time: #{varTime}"				#current time
 puts "#{e} #{varAvg.round(1)} (#{varCcum.round(1)} cumulative)"
 puts "#{f} #{varAvgt.round(1)} (#{varTcum.round(1)} cumulative)"
-puts ""
+puts ""										#space
 puts "Variable Inputs:" 					#variable inputs
 puts "Loop delay: #{d} seconds" 			#loop delay in seconds
-puts "Iterations: #{varI} of #{varNum+1}"		#x of y count
+puts "Iterations: #{varI} of #{varNum+1}"	#x of y count
 
 #WRITE TO SOMEFILE
 File.open(gname, "a+") do |g2|
@@ -154,8 +152,6 @@ File.open(hname, "a+") do |h1|
 	h1.puts "#{e} #{varAvg.round(1)} (#{varCcum.round(1)} cumulative)"
 	h1.puts "#{f} #{varAvgt.round(1)} (#{varTcum.round(1)} cumulative)"
 end
-
-
 sleep(d)
 end until varI > varNum
 
