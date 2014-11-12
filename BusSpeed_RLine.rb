b8cd6afb2bc6179f242ae3e65721d67d47d6daf6
benda18 <bendertj@gmail.com>
2014-11-12 08:05:42 -0500

    sixth commit

---
+++ BusSpeed_RLine.rb
@@ -0,0 +1,15 @@
+require 'unirest'
+require 'json'
+#fname = "sample.txt"
+fname = "R-Line #{Time.now.strftime('%Y%m%d-%H%M%S')}.txt"
+somefile = File.open(fname, "w")
+
+response = Unirest.get "https://transloc-api-1-2.p.mashape.com/vehicles.jsonp?agencies=20&callback=call&routes=4000032",
+  headers:{
+    "X-Mashape-Key" => "5SA6zZbMxpmshjhL0Y8ERCZ2WqnIp1zgKOmjsnXy5WDKmZaGC4"
+  }
+
+
+
+somefile.puts response.body
+somefile.close
