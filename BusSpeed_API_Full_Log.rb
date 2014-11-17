50ebd2d4bd196fb3876489527f9eb5e3406e4120
benda18 <bendertj@gmail.com>
2014-11-17 11:14:38 -0500

    Create BusSpeed_API_Full_Log.rb
    A script that logs the json api data in it's entirety and writes it to a text file one time.

---
+++ BusSpeed_API_Full_Log.rb
@@ -0,0 +1,16 @@
+#A scipt that logs the json api data in it's entirety  and writes it to a file one time. 
+
+require 'unirest'
+require 'json'
+#fname = "sample.txt"
+fname = "CAT #{Time.now.strftime('%Y%m%d-%H%M%S')}.txt"
+somefile = File.open(fname, "w")
+
+#<call the transloc api>
+response = Unirest.get "https://transloc-api-1-2.p.mashape.com/vehicles.jsonp?agencies=12%2C16%2C20&callback=call",
+  headers:{
+    "X-Mashape-Key" => "<key>"
+  }
+
+somefile.puts response.body
+somefile.close
