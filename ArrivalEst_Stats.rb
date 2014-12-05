887efcad2f9df2d34a83d230954c32968e90ec8d
Tim Bender <bendertj@gmail.com>
2014-12-05 15:18:45 -0500

    Update ArrivalEst_Stats.rb

--- ArrivalEst_Stats.rb
+++ ArrivalEst_Stats.rb
@@ -1,37 +1,25 @@
 # A script that does statistical analysis on an array
-
-
 require 'descriptive_statistics'
 # descriptive_statistics documentation here: https://rubygems.org/gems/descriptive_statistics
 require 'csv'
 require 'time'
 require 'date'
 #require 'DateTime'
-
 puts "\e[H\e[2J"
 varSt = Time.now 
 puts "Started: #{varSt}" 
 puts ""
-
 #vars/defs
 #SAfile = "CATAvgStopWait 20141202.txt"
 #SAfile = Dir["CATAvgStopWait*.txt"]
-
-
-
 #SETUP
 #---month---/
-
 #---hour---/
-vLow = 6		# begin of time-of-day span in 24-hr clockface hours --> 6=6:00am-6:59am, 13=1:00pm-1:59pm, etc.
-vHig = 8		# end   of time-of-day span in 24-hr clockface hours
+vLow = 7		# begin of time-of-day span in 24-hr clockface hours --> 6=6:00am, 13=1:00pm, etc.
+vHig = 7		# end   of time-of-day span in 24-hr clockface hours --> 6=6:59am, 13=1:59pm, etc.
 #/SETUP
-
-
-
 #HEADERS
 puts "Hours between #{vLow}:00 and #{vHig}:59"
-
 arr10 = []
 arr11 = []
 arr11L = []
@@ -53,7 +41,7 @@ arr70X = []
 arr8 = []
 arr22 = []
 arr3 = []
-arr55X = []
+#arr55X = []
 arr5 = []
 arrrline = []
 arr4 = []
@@ -64,7 +52,6 @@ arr1 = []
 arr19 = []
 arr54L = []
 #arr900 = []
-
 var10 = "10"
 var11 = "11"
 var11L = "11L"
@@ -86,7 +73,7 @@ var70X = "70X"
 var8 = "8"
 var22 = "22"
 var3 = "3"
-var55X = "55X"
+#var55X = "55X"
 var5 = "5"
 varrline = "r-line"
 var4 = "4"
@@ -97,7 +84,6 @@ var1 = "1"
 var19 = "19"
 var54L = "54L"
 #var900 = "900"
-
 varI = 1
 varDate=0
 varYer = 0
@@ -109,7 +95,6 @@ varRtnam = 0
 varAgc = 0
 #varRtQuery = "r-line" 	#INPUTS
 varMins = 0
-
 varct10 = 0
 varct11 = 0
 varct11L = 0
@@ -145,11 +130,142 @@ varct54L = 0
 
 Dir.glob('CATAvgStopWait*.txt') do |foo|
 
-list1 =CSV.foreach(foo, :headers => true, :return_headers => false) do |row1|
-varRtnam = 0
-varRtnam = row1["route_number"]#.to_a
+# list1 =CSV.foreach(foo, :headers => true, :return_headers => false) do |row1|
+# varRtnam = 0
+# varRtnam = row1["route_number"]#.to_a
+# if varRtnam == var10
+# varct10 +=1;
+# end
+# if varRtnam == var11
+# varct11 +=1;
+# end
+# if varRtnam == var11L
+# varct11L +=1;
+# end
+# if varRtnam == var12
+# varct12 +=1;
+# end
+# if varRtnam == var13
+# varct13 +=1;
+# end
+# if varRtnam == var15
+# varct15 +=1;
+# end
+# if varRtnam == var15L
+# varct15L +=1;
+# end
+# if varRtnam == var16
+# varct16 +=1;
+# end
+# if varRtnam == var18
+# varct18 +=1;
+# end
+# if varRtnam == var2
+# varct2 +=1;
+# end
+# if varRtnam == var21
+# varct21 +=1;
+# end
+# if varRtnam == var23L
+# varct23L +=1;
+# end
+# if varRtnam == var24L
+# varct24L +=1;
+# end
+# if varRtnam == var25L
+# varct25L +=1;
+# end
+# if varRtnam == var40X
+# varct40X +=1;
+# end
+# if varRtnam == var6
+# varct6 +=1;
+# end
+# if varRtnam == var7
+# varct7 +=1;
+# end
+# # if varRtnam == var70X
+# # varct70X +=1;
+# # end
+# if varRtnam == var8
+# varct8 +=1;
+# end
+# if varRtnam == var22
+# varct22 +=1;
+# end
+# if varRtnam == var3
+# varct3 +=1;
+# end
+# # if varRtnam == var55X
+# # varct55X +=1;
+# # end
+# if varRtnam == var5
+# varct5 +=1;
+# end
+# if varRtnam == varrline
+# varctrline +=1;
+# end
+# if varRtnam == var4
+# varct4 +=1;
+# end
+# if varRtnam == var7L
+# varct7L +=1;
+# end
+# # if varRtnam == varwfl
+# # varctwfl +=1;
+# # end
+# if varRtnam == var31
+# varct31 +=1;
+# end
+# if varRtnam == var1
+# varct1 +=1;
+# end
+# if varRtnam == var19
+# varct19 +=1;
+# end
+# if varRtnam == var54L
+# varct54L +=1;
+# end
+# # if varRtnam == var900
+# # varct900 +=1;
+# # end
+# end
+
+list =CSV.foreach(foo, :headers => true, :return_headers => false) do |row|
+#=begin
+	varDate=0
+	varYer = 0
+	varMon = 0
+	varDay = 0
+	varHr = 0
+	varMin = 0
+	varRtnam = 0
+	varAgc = 0
+	varMins = 0
+#=end
+	varMins = row["MinsToArriv"].to_f
+	if varMins > 0
+	varRtnam = row["route_number"]#.to_a
 
 
+
+
+
+#=begin
+#	varAgc = row["agency"]
+#	varSid = row["stop_id"]
+#=end
+	#PARSE TIME
+	varDate = row["gendate"]
+	varDate = DateTime.parse(varDate)
+	varDate = varDate.to_time.iso8601
+	varDate = DateTime.parse(varDate)		#time is parsed and in local (gmt-5) format
+#	varYer = varDate.year
+#	varMon = varDate.month
+#	varDay = varDate.day
+	varHr = varDate.hour.to_f
+	if varHr.between?(vLow, vHig)
+=begin	
 if varRtnam == var10
 varct10 +=1;
 end
@@ -213,9 +329,9 @@ end
 if varRtnam == var3
 varct3 +=1;
 end
-if varRtnam == var55X
-varct55X +=1;
-end
+# if varRtnam == var55X
+# varct55X +=1;
+# end
 if varRtnam == var5
 varct5 +=1;
 end
@@ -242,55 +358,13 @@ varct19 +=1;
 end
 if varRtnam == var54L
 varct54L +=1;
-end
-# if varRtnam == var900
-# varct900 +=1;
-# end
-end
-
-
-list =CSV.foreach(foo, :headers => true, :return_headers => false) do |row|
-#=begin
-	varDate=0
-	varYer = 0
-	varMon = 0
-	varDay = 0
-	varHr = 0
-	varMin = 0
-	varRtnam = 0
-	varAgc = 0
-	varMins = 0
-#=end
-	varMins = row["MinsToArriv"].to_f
-	if varMins > 0
-	varRtnam = row["route_number"]#.to_a
+end	
+=end
 	
-
-
-
-	
-	
-#=begin
-#	varAgc = row["agency"]
-#	varSid = row["stop_id"]
-#=end
-
-	#PARSE TIME
-	varDate = row["gendate"]
-	varDate = DateTime.parse(varDate)
-	varDate = varDate.to_time.iso8601
-	varDate = DateTime.parse(varDate)		#time is parsed and in local (gmt-5) format
-#	varYer = varDate.year
-#	varMon = varDate.month
-#	varDay = varDate.day
-	varHr = varDate.hour.to_f
-	if varHr.between?(vLow, vHig)
 #	varMin = varDate.minute
 	#/PARSE TIME
-	
 	#puts row["route_number"].inspect()
 	#sleep(100)
-	
 	if varRtnam == var10					#query dependencies 
 	arr10 << row[6].to_f
 	end
@@ -354,9 +428,9 @@ list =CSV.foreach(foo, :headers => true, :return_headers => false) do |row|
 	if varRtnam == var3
 	arr3 << row[6].to_f
 	end
-	if varRtnam == var55X
-	arr55X << row[6].to_f
-	end
+	# if varRtnam == var55X
+	# arr55X << row[6].to_f
+	# end
 	if varRtnam == var5
 	arr5 << row[6].to_f
 	end
@@ -392,43 +466,41 @@ end
 end
 end
 puts "Average minutes to next bus arrival by route..."
-puts "##{var1}	#{arr1.mean.round(0)}" unless varct1==0
-puts "##{var2}	#{arr2.mean.round(0)}" unless varct2==0
-puts "##{var3}	#{arr3.mean.round(0)}" unless varct3==0
-puts "##{var4}	#{arr4.mean.round(0)}" unless varct4==0
-puts "##{var5}	#{arr5.mean.round(0)}" unless varct5==0
-puts "##{var6}	#{arr6.mean.round(0)}" unless varct6==0
-puts "##{var7}	#{arr7.mean.round(0)}" unless varct7==0
-puts "##{var7L}	#{arr7L.mean.round(0)}" unless varct7L==0
-puts "##{var8}	#{arr8.mean.round(0)}" unless varct8==0
-puts "##{var10}	#{arr10.mean.round(0)}" unless varct10==0
-puts "##{var11}	#{arr11.mean.round(0)}" unless varct11==0
-puts "##{var11L}	#{arr11L.mean.round(0)}" unless varct11L==0
-puts "##{var12}	#{arr12.mean.round(0)}" unless varct12==0
-puts "##{var13}	#{arr13.mean.round(0)}" unless varct13==0
-puts "##{var15}	#{arr15.mean.round(0)}" unless varct15==0
-puts "##{var15L}	#{arr15L.mean.round(0)}" unless varct15L==0
-puts "##{var16}	#{arr16.mean.round(0)}" unless varct16==0
-puts "##{var18}	#{arr18.mean.round(0)}" unless varct18==0
-puts "##{var19}	#{arr19.mean.round(0)}" unless varct19==0
-puts "##{var21}	#{arr21.mean.round(0)}" unless varct21==0
-puts "##{var22}	#{arr22.mean.round(0)}" unless varct22==0
-puts "##{var23L}	#{arr23L.mean.round(0)}" unless varct23L==0
-puts "##{var24L}	#{arr24L.mean.round(0)}" unless varct24L==0
-puts "##{var25L}	#{arr25L.mean.round(0)}" unless varct25L==0
-#puts "##{var31}	#{arr31.mean.round(0)}" unless varct31==0
-puts "##{var40X}	#{arr40X.mean.round(0)}" unless varct40X==0
-puts "##{var54L}	#{arr54L.mean.round(0)}" unless varct54L==0
-puts "##{var55X}	#{arr55X.mean.round(0)}" unless varct55X==0
-#puts "##{var70X}	#{arr70X.mean.round(0)}" unless varct70X==0
-#puts "##{var900}	#{arr900.mean.round(0)}" unless varct900==0
-puts "##{varrline}	#{arrrline.mean.round(0)}" unless varctrline==0
-#puts "##{varwfl}	#{arrwfl.mean.round(0)}" unless varctwfl==0
-
+puts "##{var1}	#{arr1.mean.round(0)}" unless arr1==nil
+puts "##{var2}	#{arr2.mean.round(0)}" unless arr2==nil
+puts "##{var3}	#{arr3.mean.round(0)}" unless arr3==nil
+puts "##{var4}	#{arr4.mean.round(0)}" unless arr4==nil
+puts "##{var5}	#{arr5.mean.round(0)}" unless arr5==nil
+puts "##{var6}	#{arr6.mean.round(0)}" unless arr6==nil
+puts "##{var7}	#{arr7.mean.round(0)}" unless arr7==nil
+puts "##{var7L}	#{arr7L.mean.round(0)}" unless arr7L==nil
+puts "##{var8}	#{arr8.mean.round(0)}" unless arr8==nil
+puts "##{var10}	#{arr10.mean.round(0)}" unless arr10==nil
+puts "##{var11}	#{arr11.mean.round(0)}" unless arr11==nil
+puts "##{var11L}	#{arr11L.mean.round(0)}" unless arr11L==nil
+puts "##{var12}	#{arr12.mean.round(0)}" unless arr12==nil
+puts "##{var13}	#{arr13.mean.round(0)}" unless arr13==nil
+puts "##{var15}	#{arr15.mean.round(0)}" unless arr15==nil
+puts "##{var15L}	#{arr15L.mean.round(0)}" unless arr15L==nil
+puts "##{var16}	#{arr16.mean.round(0)}" unless arr16==nil
+puts "##{var18}	#{arr18.mean.round(0)}" unless arr18==nil
+puts "##{var19}	#{arr19.mean.round(0)}" unless arr19==nil
+puts "##{var21}	#{arr21.mean.round(0)}" unless arr21==nil
+puts "##{var22}	#{arr22.mean.round(0)}" unless arr22==nil
+puts "##{var23L}	#{arr23L.mean.round(0)}" unless arr23L==nil
+puts "##{var24L}	#{arr24L.mean.round(0)}" unless arr24L==nil
+puts "##{var25L}	#{arr25L.mean.round(0)}" unless arr25L==nil
+#puts "##{var31}	#{arr31.mean.round(0)}" unless arr31==nil
+puts "##{var40X}	#{arr40X.mean.round(0)}" unless arr40X==nil
+puts "##{var54L}	#{arr54L.mean.round(0)}" unless arr54L==nil
+#puts "##{var55X}	#{arr55X.mean.round(0)}" unless arr55X==nil
+#puts "##{var70X}	#{arr70X.mean.round(0)}" unless arr70X==nil
+#puts "##{var900}	#{arr900.mean.round(0)}" unless arr900==nil
+puts "##{varrline}	#{arrrline.mean.round(0)}" unless arrrline==nil
+#puts "##{varwfl}	#{arrwfl.mean.round(0)}" unless arrwfl==nil
 varEn = Time.now
 varDur = varEn - varSt	#seconds
 varDur = varDur / 60	#minutes
 puts ""
 puts "Duration: #{varDur.round(1)} minutes"
-
 exit
